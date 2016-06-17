a52753d8b0e74a4e22ef635fc3096785c4e964fd
reedbr <reed.br@husky.neu.edu>
2016-06-16 22:57:30 -0400

    beats are gucci

--- src/cs3500/music/view/ConcreteGuiViewPanel.java
+++ src/cs3500/music/view/ConcreteGuiViewPanel.java
@@ -22,8 +22,96 @@ public class ConcreteGuiViewPanel extends JPanel {
   private IMusicModel m = new MusicModel();
 
   public void initData() {
-    this.m.write(Tone.A, 8, 3);
-    this.m.write(Tone.B, 6, 3);
+    m.write(Tone.G, 7, 3);
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.C, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 7, 3);
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 3, 4);
+    m.advance();
+    m.advance();
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 8, 3);
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 4, 4);
+    m.advance();
+    m.advance();
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 2, 3);
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 4, 4);
+    m.advance();
+    m.advance();
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 8, 3);
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.C, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 8, 3);
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.G, 8, 3);
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.D, 2, 4);
+    m.advance();
+    m.advance();
+    m.write(Tone.E, 8, 3);
+    m.write(Tone.C, 8, 4);
   }
 
 
@@ -37,27 +125,37 @@ public class ConcreteGuiViewPanel extends JPanel {
     // and methods on it that may be useful
     this.initData();
     List<List<Note>> curMusic = m.getMusic();
+    ArrayList<ArrayList<Note>> arrCur = new ArrayList<>();
+
+    for (int a = 0; a < curMusic.size(); a++) {
+      arrCur.add(new ArrayList<>());
+      for (int z = 0; z < arrCur.get(a).size(); z++) {
+        arrCur.get(a).add((Note) curMusic.get(a).get(z));
+      }
+    }
+
     int totalTime = m.getTotalTime();
+    int first = 50;
     for (int i = 0; i < totalTime; i += 16) {
       String temp = Integer.toString(i);
-      g.drawString(temp, 50 + i, 25);
+      g.drawString(temp, first + i, 25);
 
       for (int x = 50; x < 900; x += 60) {
         for (int y = 30; y < 275; y += 20) {
           g.drawRect(x, y, 60, 20);
-
         }
       }
+      first += 220;
     }
 
     ArrayList<Note> curTones = new ArrayList<>();
 
-    for (int i = 0; i < curMusic.size(); i++) {
-      if (curMusic.get(i).isEmpty()) {
+    for (int h = 0; h < curMusic.size(); h++) {
+      if (curMusic.get(h).isEmpty()) {
 
       } else {
-        Note temp = curMusic.get(i).get(0);
-        curTones.add(temp);
+        Note tempTone = curMusic.get(h).get(0);
+        curTones.add(tempTone);
       }
     }
 
@@ -65,18 +163,10 @@ public class ConcreteGuiViewPanel extends JPanel {
     for (Note n : curTones) {
       String tone = n.getTone().getString();
       String oct = Integer.toString(n.getOctave());
-      String temp = tone + oct;
-      g.drawString(temp, 25, j);
+      String tempStr = tone + oct;
+      g.drawString(tempStr, 25, j);
       j += 20;
     }
-
-
-
-
-
-
-
-
 
   }
 
